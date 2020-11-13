9b4c39935cae783c4b82969e60c300b16b8ec4ce
Pradeep-Gopal <pradeepprachu@gmail.com>
2020-11-13 10:57:24 -0500

    Alphabet rangoli

---
+++ alphabet_rangoli.py
@@ -0,0 +1,15 @@
+from pip._vendor.distlib.compat import raw_input
+
+
+
+def print_rangoli(size):
+    myStr = 'abcdefghijklmnopqrstuvwxyz'[0:size]
+
+    for i in range(size - 1, -size, -1):
+        x = abs(i)
+        line = myStr[size:x:-1] + myStr[x:size]
+        print("--" * x + '-'.join(line) + "--" * x)
+
+if __name__ == '__main__':
+    n = int(raw_input())
+    print_rangoli(n)
